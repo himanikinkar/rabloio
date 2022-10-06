af2ef48df8d35221aec8bcc7ab3c14d18f1c1975
Himani <himanikinkar02@gmail.com>
2022-10-06 13:18:14 +0530

    fjsdfhjdfhhgh

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authBook = require("./routes/auth");
 const updateBook = require("./routes/books");
+
 
 dotenv.config();
 
@@ -14,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL, {
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authBook);
 app.use("/api/books", updateBook);
